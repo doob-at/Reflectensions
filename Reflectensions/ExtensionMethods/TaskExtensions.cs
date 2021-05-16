b7605bd852151e3cdb359a4e17a6c6cd8c55369d
windischb <bernhard.windisch@doob.at>
2021-05-16 15:16:39 +0200

    refactor ConvertToTaskOf

---
+++ Reflectensions/ExtensionMethods/TaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using doob.Reflectensions.ExtensionMethods;
+
+namespace doob.Reflectensions.Internal
+{
+    public static class TaskExtensions
+    {
+        public static async Task<TResult?> ConvertToTaskOf<TResult>(this Task task) =>
+            await task.ContinueWith(t => 
+                t.Reflect().GetPropertyValue<TResult>("Result")
+            );
+    }
+}
