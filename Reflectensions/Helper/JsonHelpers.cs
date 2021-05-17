157d38cb416614a45b85a0bacb88fe842929bae6
windischb <bernhard.windisch@doob.at>
2021-05-17 19:49:52 +0200

    Add Reflectensions.Common with IJson Interface

---
+++ Reflectensions/Helper/JsonHelpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Reflectensions;
+
+namespace doob.Reflectensions.Helper
+{
+    public static class JsonHelpers
+    {
+
+        private static IJson? _json = null;
+
+        static JsonHelpers()
+        {
+            Initialize();
+        }
+
+        private static bool _initialized = false;
+        static void Initialize()
+        {
+
+            if (_initialized)
+            {
+                return;
+            }
+            _initialized = true;
+
+            var _jsonType = TypeHelper.FindType("doob.Reflectensions.Json");
+
+            if (_jsonType != null)
+            {
+                _json = (IJson)Activator.CreateInstance(_jsonType, new object[] { true });
+            }
+
+        }
+
+        public static bool IsAvailable()
+        {
+            Initialize();
+            return _json != null;
+        }
+
+        public static IJson Json()
+        {
+            Initialize();
+
+            return _json;
+        }
+
+    }
+
+}
