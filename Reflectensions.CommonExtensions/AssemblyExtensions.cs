1bb693e9fbc9c8edfd2f455366f6341e2103f24d
windischb <bernhard.windisch@doob.at>
2023-04-02 11:44:24 +0200

    add resourcehelpers

---
+++ Reflectensions.CommonExtensions/AssemblyExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace doob.Reflectensions.Common
+{
+    public static class AssemblyExtensions
+    {
+        public static Stream? ReadResourceAsStream(this Assembly assembly, string fileName)
+        {
+            return assembly.GetManifestResourceStream(fileName);
+        }
+        
+        public static string? ReadResourceAsString(this Assembly assembly, string fileName)
+        {
+            using var stream = assembly.ReadResourceAsStream(fileName);
+            if (stream == null)
+                return null;
+            using var streamReader = new StreamReader(stream);
+            return streamReader.ReadToEnd();
+        }
+
+        public static byte[]? ReadResourceAsByteArray(this Assembly assembly, string fileName)
+        {
+            using var stream = assembly.ReadResourceAsStream(fileName);
+            if (stream == null)
+                return null;
+
+            var memoryStream = new MemoryStream();
+            stream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
+
+    }
+}
